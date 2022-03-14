386b2e71960e74188bb968cb324f061a4e67b37e
nuomegaalpha <59454442+nuomegaalpha@users.noreply.github.com>
2022-03-14 11:39:32 -0400

    Update init.m
    added jsonlab add to path

---
+++ init.m
@@ -0,0 +1,20 @@
+clear;clf;close all;clc;
+% This script must be run first when in the directory if one wants to use
+% hmnl-scattering-film-measurement. Make sure that you are in the directory
+% hmnl-scattering-film-measurement
+
+% jsonlab information
+% Name: jsonlab
+% Version: 2.0
+% Date: 2020-13-06
+% Title: A JSON/UBJSON/MessagePack encoder/decoder for MATLAB/Octave
+% Author: Qianqian Fang <fangqq@gmail.com>
+% Maintainer: Qianqian Fang <fangqq@gmail.com>
+
+% Get current path (pwd = present working directory)
+paths = fullfile(pwd, {'jsonlab-2.0'});
+
+% add paths to MATLAB search path
+for i = 1:length(paths)
+    addpath(paths{i});
+end
