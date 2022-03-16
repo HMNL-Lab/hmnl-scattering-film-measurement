f7768b20c1d3e89272ff5abe04622bd3c2a9c418
nuomegaalpha <59454442+nuomegaalpha@users.noreply.github.com>
2022-03-16 15:26:05 -0400

    Update loadBatch.m
    added loading function for batch of csvs

--- functions/loadBatch.m
+++ functions/loadBatch.m
@@ -1,5 +1,14 @@
 function [batch] = loadBatch()
 %LOADBATCH Summary of this function goes here
 %   Detailed explanation goes here
+[files, path] = uigetfile('*.csv', 'Select batch CSV file: ','MultiSelect','off');
+if isequal(files,0)
+    disp('User selected Cancel');
+else
+    fullpath = fullfile(path, files);
+    raw = importdata(fullpath);
+    batch = raw(2:end);
+    batch = convertCharsToStrings(batch);
+end
 end
 
