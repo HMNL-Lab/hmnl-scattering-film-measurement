52b4d25b3237fb331e8a7e6a624c295cfd0f1b35
nuomegaalpha <59454442+nuomegaalpha@users.noreply.github.com>
2022-03-16 15:18:30 -0400

    Update main.m

--- main.m
+++ main.m
@@ -6,4 +6,6 @@ run('startup.m')
 batch = intializeImages();
 %% Step 2b. Load images from batch CSV file.
 
-%% Step 3. Canny Method - Assumes that images have been corrected in their rotation
+%% Step 3a. Canny Method
+% Assumes that images have been corrected in their rotation so that the
+% spray is vertical
