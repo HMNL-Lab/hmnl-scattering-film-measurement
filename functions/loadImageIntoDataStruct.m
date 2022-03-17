7ade7332fac7285f5423b966327412dc692cd502
nuomegaalpha <59454442+nuomegaalpha@users.noreply.github.com>
2022-03-17 12:12:46 -0400

    Update loadImageIntoDataStruct.m
    comments for clarity

--- functions/loadImageIntoDataStruct.m
+++ functions/loadImageIntoDataStruct.m
@@ -4,10 +4,12 @@ data = loadjson(jsonFilePath);
 data_fnames = convertCharsToStrings(fieldnames(data));
 % fields that are bright/dark field microscope modes ie "bf" or "df"
 modes_idx = "bf" == data_fnames | "df" == data_fnames;
+% throw error if no fields are "bf" or "df"
 if modes_idx == zeros(length(modes_idx), 1)
     msg = join(["Struct in file", file, "does not have 'bf' or 'df' has header fields. Please check JSON files."], " ");
     error(msg);
 end
+% loop over field names, and then read image into data struct
 for i = 1:length(modes_idx)
     if modes_idx(i)
         subfields = convertCharsToStrings(fieldnames(data.(data_fnames(i))));
