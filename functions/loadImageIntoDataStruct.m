80555b49545661aa57278d6fbaed3df5d10e5e05
nuomegaalpha <59454442+nuomegaalpha@users.noreply.github.com>
2022-03-23 13:02:23 -0400

    throw error if no "reflect" or "trans" fields

--- functions/loadImageIntoDataStruct.m
+++ functions/loadImageIntoDataStruct.m
@@ -6,15 +6,20 @@ data_fnames = convertCharsToStrings(fieldnames(data));
 modes_idx = "bf" == data_fnames | "df" == data_fnames;
 % throw error if no fields are "bf" or "df"
 if modes_idx == zeros(length(modes_idx), 1)
-    msg = join(["Struct in file", file, "does not have 'bf' or 'df' has header fields. Please check JSON files."], " ");
+    msg = join(["Struct in file", jsonFilePath, "does not have 'bf' or 'df' has header fields. Please check JSON files."], " ");
     error(msg);
 end
 % loop over field names, and then read image into data struct
 for i = 1:length(modes_idx)
     if modes_idx(i)
         subfields = convertCharsToStrings(fieldnames(data.(data_fnames(i))));
+        subfields_idx = "reflect" == subfields | "trans" == subfields;
+        if subfields_idx == zeros(length(subfields_idx), 1)
+            msg = join(["Sub-struct in", modes_idx(i), "in file", jsonFilePath, "does not contain fields 'reflect' or 'trans'. Please check JSON files."], " ");
+            error(msg);
+        end
         for j = 1:length(subfields)
-            if strcmp(subfields(j), "reflect") | strcmp(subfields(j), "trans")
+            if subfields_idx(j)
                 data.(data_fnames(i)).(subfields(j)).Image = imread(data.(data_fnames(i)).(subfields(j)).Filename);
                 isImageExist.(data_fnames(i)).(subfields(j)) = true;
             end
