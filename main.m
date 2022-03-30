5e6812ea7e2e2d1780f8a62100c68183af94d8b0
nuomegaalpha <59454442+nuomegaalpha@users.noreply.github.com>
2022-03-29 22:32:51 -0400

    Update main.m

--- main.m
+++ main.m
@@ -12,4 +12,16 @@ batch = loadBatch();
 % spray is vertical
 % First, load user selected positioning information into JSON files
 userCoordsToJSON();
-%% Step 3a.2.
+%% Step 3a.2. Batch process files contained in variable 'batch'
+% set options used in Canny method
+
+% search width
+opts.width = 40;
+% threshold
+opts.cannyThreshold = [0.01 0.2];
+% Gaussian standard deviation of filter
+opts.cannyStd = 10;
+for i = length(batch)
+    % data is saved to same location as selected file
+    [~] = cannyThickness(batch(i), opts);
+end
