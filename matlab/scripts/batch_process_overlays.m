e354da984429069ac0922e8885b487d1ae618dc0
nuomegaalpha <59454442+nuomegaalpha@users.noreply.github.com>
2022-08-01 11:56:23 -0400

    create script for batch processing overlays

---
+++ matlab/scripts/batch_process_overlays.m
@@ -0,0 +1,24 @@
+dir = uigetdir('C:\Users\noahm\Documents\Rutgers\HMNL\hmnl-scattering-film-measurement\test-scripts\tmp\', 'Select directory to save new overlay image files to');
+[files, path] = uigetfile({'*.json;*.h5;*.hdf5'},'Select optical thickness files: ', 'MultiSelect','on');
+
+for i = 1:length(files)
+    fullpath = string(fullfile(path, files(i)));
+    [folder, sample_name, ext] = fileparts(fullpath);
+    data = read_data_from_h5_or_json(fullpath);
+    image = overlay_edges(data);
+    save_to = fullfile(folder, strjoin([sample_name, ".tif"], ''));
+    if isfile(save_to)
+        warning('File exists.')
+        str = input('[Y/N] to overwrite: ', 's');
+        if str == 'Y'
+            imwrite(image, save_to);
+            fprintf('Sample %s successfully saved! \nSaved to %s \n\n', sample_name, save_to);
+        else
+            disp('Operation cancelled.');
+        end
+    else
+        imwrite(image, save_to);
+        fprintf('Sample %s successfully saved! \nSaved to %s \n\n', sample_name, save_to);
+    end
+    clear image;
+end
